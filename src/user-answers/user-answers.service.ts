5c16d9a8c7330b8a588f0f0d9a48b2f3796556af
dwarowski <realcarenthusiast@yandex.ru>
2025-02-23 14:29:45 +0300

    Update user-answers.service.ts

--- src/user-answers/user-answers.service.ts
+++ src/user-answers/user-answers.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+import { UserAnswersEntity } from './dto/user-answer.entity';
 
 @Injectable()
-export class UserAnswersService {}
+export class UserAnswersService {
+    constructor(
+        @InjectRepository(UserAnswersEntity)
+        private repository: Repository<UserAnswersEntity>
+    ) {}
+
+    getTags(){
+            return this.repository.find()
+        }
+    
+    createTag(dto: CreateProblemDto){
+        return this.repository.save(dto)
+
+}
