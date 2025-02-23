72e4cf77bfc76ea605df232856f9ba541e5af9f9
dwarowski <realcarenthusiast@yandex.ru>
2025-02-23 17:24:07 +0300

    Finished answers module

--- src/answers/answers.controller.ts
+++ src/answers/answers.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+
+import {
+    Get,
+    Post,
+    Body,
+    Controller,
+} from '@nestjs/common';
+import { AnswersService } from './answers.service';
+import { CreateAnswerDto } from './dto/create-answer.dto';
 
 @Controller('answers')
-export class AnswersController {}
+@ApiTags('answers')
+export class AnswersController {
+    constructor(private readonly AnswersService: AnswersService) {}
+
+    @Get()
+    getAnswers() {
+        return this.AnswersService.getAnswers();
+    }
+
+    @Post()
+    createAnswer(@Body() dto: CreateAnswerDto) {
+        return this.AnswersService.createAnswer(dto);
+    }
+}
--- src/answers/answers.service.ts
+++ src/answers/answers.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AnswersEntity } from './entity/answers.entity';
+import { Repository } from 'typeorm';
+import { CreateAnswerDto } from './dto/create-answer.dto';
 
 @Injectable()
-export class AnswersService {}
+export class AnswersService {
+    constructor(
+        @InjectRepository(AnswersEntity)
+        private repository: Repository<AnswersEntity>
+    ) {}
+    getAnswers(){
+            return this.repository.find()
+        }
+    
+    createAnswer(dto: CreateAnswerDto){
+        return this.repository.save(dto)
+    }
+}
