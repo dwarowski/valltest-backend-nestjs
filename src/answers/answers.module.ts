d7932155acc4f3bbcb9dc47dc3f5d1716cc0b551
dwarowski <realcarenthusiast@yandex.ru>
2025-02-23 16:20:21 +0300

    made import of entites

--- src/answers/answers.module.ts
+++ src/answers/answers.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AnswersEntity } from './entity/answers.entity';
+import { UserAnswersController } from 'src/user-answers/user-answers.controller';
+import { UserAnswersService } from 'src/user-answers/user-answers.service';
 
-@Module({})
+@Module({
+    imports: [ConfigModule, TypeOrmModule.forFeature([AnswersEntity])],
+    controllers: [UserAnswersController],
+    providers: [UserAnswersService]
+})
 export class AnswersModule {}
--- src/user-answers/user-answers.module.ts
+++ src/user-answers/user-answers.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserAnswersController } from './user-answers.controller';
 import { UserAnswersService } from './user-answers.service';
+import { UserAnswersEntity } from './entity/user-answer.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule, TypeOrmModule.forFeature([UserAnswersEntity])],
   controllers: [UserAnswersController],
   providers: [UserAnswersService]
 })
