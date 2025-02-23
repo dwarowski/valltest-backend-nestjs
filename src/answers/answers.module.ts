4254773f25993192fd04980aaad0a4de50b24690
dwarowski <realcarenthusiast@yandex.ru>
2025-02-23 16:23:12 +0300

    fixed bug

--- src/answers/answers.module.ts
+++ src/answers/answers.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnswersEntity } from './entity/answers.entity';
-import { UserAnswersController } from 'src/user-answers/user-answers.controller';
-import { UserAnswersService } from 'src/user-answers/user-answers.service';
+import { AnswersController } from './answers.controller';
+import { AnswersService } from './answers.service';
 
 @Module({
     imports: [ConfigModule, TypeOrmModule.forFeature([AnswersEntity])],
-    controllers: [UserAnswersController],
-    providers: [UserAnswersService]
+    controllers: [AnswersController],
+    providers: [AnswersService]
 })
 export class AnswersModule {}
