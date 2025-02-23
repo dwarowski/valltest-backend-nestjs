8b41c385dbe0fa11ec0ab2956287702f8f4aeb53
dwarowski <realcarenthusiast@yandex.ru>
2025-02-23 14:55:05 +0300

    working contoller

--- src/user-answers/user-answers.controller.ts
+++ src/user-answers/user-answers.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+
+import {
+    Get,
+    Post,
+    Body,
+    Controller 
+} from '@nestjs/common';
+import { UserAnswersService } from './user-answers.service';
+import { CreateUserAnswerDto } from './dto/create-user-answer.dto';
 
 @Controller('user-answers')
-export class UserAnswersController {}
+@ApiTags('user-answwers')
+export class UserAnswersController {
+    constructor(private readonly UserAnswersService: UserAnswersService) {}
+
+    @Get()
+    getUserAnswersByUser() {
+        return this.UserAnswersService.getUserAnswersByUser();
+    }
+
+    @Post()
+    createUserAnswer(@Body() dto: CreateUserAnswerDto) {
+        return this.UserAnswersService.createUserAnswer(dto);
+    }
+
+}
