dfa9bbc967d9224ce060b6977ae89a2720d9b9c7
dwarowski <realcarenthusiast@yandex.ru>
2025-02-24 15:43:58 +0300

    Update postgres.config.ts

--- src/configs/postgres.config.ts
+++ src/configs/postgres.config.ts
@@ -7,7 +7,7 @@ export const getPostgresConfig = async (
   return {
     type: 'postgres',
     host: configService.get('DATABASE_HOST'),
-    port: parseInt(configService.get('DATABASE_PORT')  || '5432', 10),
+    port: configService.get('DATABASE_PORT'),
     username: configService.get('DATABASE_USERNAME'),
     password: configService.get('DATABASE_PASSWORD'),
     database: configService.get('DATABASE_NAME'),
