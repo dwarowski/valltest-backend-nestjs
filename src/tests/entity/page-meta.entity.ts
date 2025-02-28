182186680cc1b100b2a71eecfbc352252ed30356
dwarowski <realcarenthusiast@yandex.ru>
2025-03-01 00:49:07 +0300

    triyng to add pagination

--- src/tests/entity/page-meta.entity.ts
+++ src/tests/entity/page-meta.entity.ts
@@ -1,3 +1,17 @@
 export class PageMetaEntity{
-    
+    page: number;
+
+    take: number;
+
+    itemCount: number;
+
+    pageCount: number;
+
+    hasPreviousPage: boolean;
+
+    hasNextPage: boolean;
+
+    constructor(page) {
+
+    }
 }
