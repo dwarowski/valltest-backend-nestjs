23bc8247352ad22875e56d55c0b6fb2cfabd6949
dwarowski <realcarenthusiast@yandex.ru>
2025-03-01 00:45:21 +0300

    page entity finished

--- src/tests/entity/page.entity.ts
+++ src/tests/entity/page.entity.ts
@@ -1,3 +1,15 @@
+import { IsArray } from "class-validator";
+import { PageMetaEntity } from "./page-meta.entity";
+
 export class PageEntity {
     
+    meta: PageMetaEntity;
+    
+    @IsArray()
+    data: [];
+
+    constructor(data: [], meta: PageMetaEntity){
+        this.data = data;
+        this.meta = meta;
+    }
 }
