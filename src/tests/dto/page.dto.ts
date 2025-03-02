5723ef2bdc3ea35ba774d5f4dce4631201c96fd4
dwarowski <realcarenthusiast@yandex.ru>
2025-03-02 12:44:42 +0300

    made variables readonly

--- src/tests/dto/page.dto.ts
+++ src/tests/dto/page.dto.ts
@@ -3,11 +3,11 @@ import { PageMetaDto } from "./page-meta.dto";
 
 
 export class PageDto {
-    
-    meta: PageMetaDto;
+
+    readonly meta: PageMetaDto;
     
     @IsArray()
-    data: [];
+    readonly data: [];
 
     constructor(data: [], meta: PageMetaDto){
         this.data = data;
