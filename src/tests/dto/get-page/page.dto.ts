abbd9044217d2d71e772698b9c1c58e0d64f777b
dwarowski <realcarenthusiast@yandex.ru>
2025-03-02 13:16:13 +0300

    class is now generic

--- src/tests/dto/get-page/page.dto.ts
+++ src/tests/dto/get-page/page.dto.ts
@@ -2,14 +2,14 @@ import { IsArray } from "class-validator";
 import { PageMetaDto } from "./page-meta.dto";
 
 
-export class PageDto {
+export class PageDto<T> {
 
     readonly meta: PageMetaDto;
     
     @IsArray()
-    readonly data: [];
+    readonly data: T[];
 
-    constructor(data: [], meta: PageMetaDto){
+    constructor(data: T[], meta: PageMetaDto){
         this.data = data;
         this.meta = meta;
     }
