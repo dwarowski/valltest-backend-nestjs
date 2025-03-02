4e7a28e9e9303eb33b804811baab91112f3a15da
dwarowski <realcarenthusiast@yandex.ru>
2025-03-02 12:36:13 +0300

    Create page-meta.dto.ts

---
+++ src/tests/dto/page-meta.dto.ts
@@ -0,0 +1,24 @@
+import { PageMetaDtoParams } from "./page-meta-params.dto";
+
+export class PageMetaDto{
+    page: number;
+
+    take: number;
+
+    itemCount: number;
+
+    pageCount: number;
+
+    hasPreviousPage: boolean;
+
+    hasNextPage: boolean;
+
+    constructor({ pageOptionsDto, itemCount }: PageMetaDtoParams) {
+        this.page = pageOptionsDto.data;
+        this.take = pageOptionsDto.take;
+        this.itemCount = itemCount;
+        this.pageCount = Math.ceil(this.itemCount / this.take);
+        this.hasPreviousPage = this.page > 1;
+        this.hasNextPage = this.page < this.pageCount;
+    }
+}
