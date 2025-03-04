4a9d2181aed1accd5d1670e1b1225f9cc5ed51cd
bulkaxleba09 <o.otarii@yandex.ru>
2025-03-05 00:43:30 +0300

    update profile
    регистраиця, автторизаиця, получение профиля по айди + обновление профиля

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,17 @@
 // src/users/users.controller.ts
-import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
+import { 
+    Controller, 
+    Get, 
+    Param, 
+    Patch,
+    Body,
+    NotFoundException,
+    UsePipes,
+    ValidationPipe,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UserController {
@@ -10,5 +20,14 @@ export class UserController {
   @Get(':id')
   async getProfile(@Param('id') id: string): Promise<User> {
     return this.usersService.findOneById(id);
+  }
+
+  @Patch(':id')
+  @UsePipes(new ValidationPipe({ transform: true })) // Включаем валидацию
+  async updateProfile(
+    @Param('id') id: string,
+    @Body() updateData: UpdateUserDto, // Используем DTO для валидации
+  ): Promise<User> {
+    return this.usersService.updateUser(id, updateData);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -17,5 +18,18 @@ export class UserService {
       throw new NotFoundException('User not found');
     }
     return user;
+  }
+
+  async updateUser(id: string, updateData: UpdateUserDto): Promise<User> {
+    const user = await this.usersRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    // Обновляем данные пользователя
+    Object.assign(user, updateData);
+
+    // Сохраняем обновленного пользователя в базе данных
+    return this.usersRepository.save(user);
   }
 }
