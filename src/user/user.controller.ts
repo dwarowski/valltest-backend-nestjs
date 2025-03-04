9f924df2fde4cb38b2deebd47ec4f3d9854fb39c
bulkaxleba09 <o.otarii@yandex.ru>
2025-03-05 00:31:34 +0300

    get user id

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+// src/users/users.controller.ts
+import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './entities/user.entity';
 
+@Controller('users')
+export class UserController {
+  constructor(private readonly usersService: UserService) {}
 
-@ApiTags('users')
-@Controller('user')
-export class UserController {}
+  @Get(':id')
+  async getProfile(@Param('id') id: string): Promise<User> {
+    return this.usersService.findOneById(id);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+// src/users/users.service.ts
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findOneById(id: string): Promise<User> {
+    const user = await this.usersRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+}
