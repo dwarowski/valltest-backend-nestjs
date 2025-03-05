a6d2a87ef01e5b140baf6d3c1b75da705cbf50fd
dwarowski <realcarenthusiast@yandex.ru>
2025-03-05 12:52:03 +0300

    import raiting service and entity

--- src/tests/tests.module.ts
+++ src/tests/tests.module.ts
@@ -5,11 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TestsController } from './tests.controller';
 import { TestsService } from './tests.service';
 import { TestsEntity } from './entity/test.entity';
+import { RatingService } from 'src/ratings/rating.service';
+import { RatingEntity } from 'src/ratings/entity/rating.entity';
 
 
 @Module({
-  imports: [ConfigModule, TypeOrmModule.forFeature([TestsEntity])],
+  imports: [ConfigModule, TypeOrmModule.forFeature([TestsEntity, RatingEntity])],
   controllers: [TestsController],
-  providers: [TestsService]
+  providers: [TestsService, RatingService]
 })
 export class TestsModule {}
