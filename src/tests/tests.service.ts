b1a2a8edb18e51f35a7923694981d294ec7bd8f4
dwarowski <realcarenthusiast@yandex.ru>
2025-03-09 18:08:46 +0300

    test creation working properly

--- src/tests/tests.service.ts
+++ src/tests/tests.service.ts
@@ -8,30 +8,59 @@ import { PageMetaDto } from '../global-dto/get-page/page-meta.dto';
 import { PageDto } from '../global-dto/get-page/page.dto';
 import { RatingService } from 'src/ratings/rating.service';
 import { UpdateTestDto } from './dto/update-test.dto';
+import { TestFilterDto } from './dto/test-filter.dto';
+import { TopicsService } from 'src/topics/topics.service';
 
 @Injectable()
 export class TestsService {
     constructor(
-        @Inject(RatingService) private readonly ratingService: RatingService,
+        @Inject(RatingService) 
+        private readonly ratingService: RatingService,
         @InjectRepository(TestsEntity)
         private repository: Repository<TestsEntity>,
+        @Inject(TopicsService)
+        private readonly topicService: TopicsService
       ) {}
 
     async getTestById(id: number){
-        console.log(id);
         return await this.repository.createQueryBuilder('test')
         .where({id: id})
         .getOne()
     }
 
-    async getTestsByPage(page: number, take: number): Promise<PageDto<TestsEntity>> {
+    async getTestsByPage(page: number, take: number, filterDto: TestFilterDto): Promise<PageDto<TestsEntity>> {
         if (isNaN(page) || isNaN(take) || take > 60 || page < 0) {
             throw new BadRequestException('Invalid pagination params')   
         }
-        const [tests, total] = await this.repository.createQueryBuilder('tests')
+
+        const testsQuery = this.repository.createQueryBuilder('tests')
         .skip((page - 1) * take)
         .take(take)
-        .getManyAndCount();
+        .leftJoinAndSelect('tests.topic', 'topic');
+
+        const { subject, topic } = filterDto;
+
+
+        console.log(await this.repository.createQueryBuilder('tests')
+        .skip((page - 1) * take)
+        .take(take)
+        .leftJoinAndSelect('tests.topic', 'topic')
+        .leftJoinAndSelect('topic.subject', 'subject')
+        .getMany())
+
+        if (topic){
+            testsQuery
+            .andWhere("topic.topicName  = :topic", { topic })
+        }
+
+        if (subject){
+            testsQuery
+            .andWhere("subject.subjectName  = :subject", { subject })
+            
+        }
+        
+
+        const [tests, total]  = await testsQuery.getManyAndCount();
 
         const testsWithRating = this.addAverageRatingToTests(tests);
 
@@ -56,8 +85,21 @@ export class TestsService {
           }));
     }
 
-    creatTest(dto: CreateTestDto){
-        return this.repository.save(dto)
+    async creatTest(dto: CreateTestDto){
+        const { testName, userAuthorId, difficulty, topicId, timeForTest } = dto;
+        
+        const topic = await this.topicService.getTopicById(topicId);
+        
+        if (!topic) {
+            throw new Error(`Topic with ID ${topicId} not found`);
+        }
+        return await this.repository.save({
+            testName,
+            userAuthorId,
+            difficulty, 
+            topic: topic,
+            timeForTest
+        })
     }
 
     async deleteTest(id: number){
