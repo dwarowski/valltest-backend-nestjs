beca76df420a5268fbe00acfd361a97380b21d4e
dwarowski <realcarenthusiast@yandex.ru>
2025-03-09 19:23:29 +0300

    update creation of topics

--- src/topics/topics.service.ts
+++ src/topics/topics.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { TopicEntity } from './entity/topic.entity';
 import { CreateTopicDto } from './dto/create-topic.dto';
+import { SubjectsService } from 'src/subjects/subjects.service';
 
 @Injectable()
 export class TopicsService {
     constructor(
+        @Inject(SubjectsService)
+        private subjectService: SubjectsService, 
         @InjectRepository(TopicEntity)
         private repository: Repository<TopicEntity>,
       ) {}
@@ -22,8 +25,19 @@ export class TopicsService {
         .getOne()
     }
 
-    createTopic(dto: CreateTopicDto){
-        return this.repository.save(dto)
+    async createTopic(dto: CreateTopicDto){
+        const { topicName, subjectId } = dto;
+        const subject = await this.subjectService.getSubjectById(subjectId)
+        if (!subject){
+            throw new BadRequestException(`Subject with ID ${subjectId} not found`)
+        }
+
+        return await this.repository.save(
+            {
+                topicName,
+                subject: subject
+            }
+        )
     }
     
 }
