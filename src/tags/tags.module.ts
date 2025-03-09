71b5234267405e5aeba743a70e8b992eb3161b54
dwarowski <realcarenthusiast@yandex.ru>
2025-03-09 20:36:55 +0300

    new imports

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -4,10 +4,18 @@ import { TagsEntity } from './entity/tags.entity';
 import { ConfigModule } from '@nestjs/config';
 import { TagsController } from './tags.controller';
 import { TagsService } from './tags.service';
+import { TestsEntity } from 'src/tests/entity/test.entity';
+import { TestsService } from 'src/tests/tests.service';
+import { RatingService } from 'src/ratings/rating.service';
+import { TopicsService } from 'src/topics/topics.service';
+import { RatingEntity } from 'src/ratings/entity/rating.entity';
+import { TopicEntity } from 'src/topics/entity/topic.entity';
+import { SubjectsService } from 'src/subjects/subjects.service';
+import { SubjectEntity } from 'src/subjects/entity/subject.entity';
 
 @Module({
-  imports: [ConfigModule, TypeOrmModule.forFeature([TagsEntity])],
+  imports: [ConfigModule, TypeOrmModule.forFeature([TagsEntity, TestsEntity, RatingEntity, TopicEntity, SubjectEntity])],
   controllers: [TagsController],
-  providers: [TagsService]
+  providers: [TagsService, TestsService, RatingService, TopicsService, SubjectsService]
 })
-export class TagsModule {}
+export class TagsModule { }
