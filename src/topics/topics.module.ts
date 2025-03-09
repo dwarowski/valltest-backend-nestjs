6d84a7fb5ce6ecc7874883e945ed9e6f2cb42f52
dwarowski <realcarenthusiast@yandex.ru>
2025-03-09 19:22:59 +0300

    subject import

--- src/topics/topics.module.ts
+++ src/topics/topics.module.ts
@@ -5,10 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TopicEntity } from './entity/topic.entity';
 import { TopicsController } from './topics.controller';
 import { TopicsService } from './topics.service';
+import { SubjectEntity } from 'src/subjects/entity/subject.entity';
+import { SubjectsService } from 'src/subjects/subjects.service';
 
 @Module({
-  imports: [ConfigModule, TypeOrmModule.forFeature([TopicEntity])],
+  imports: [ConfigModule, TypeOrmModule.forFeature([TopicEntity, SubjectEntity])],
   controllers: [TopicsController],
-  providers: [TopicsService]
+  providers: [TopicsService, SubjectsService]
 })
 export class TopicsModule {}
