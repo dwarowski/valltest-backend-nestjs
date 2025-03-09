2ecbbd2e53a90dc307f2f9d700317f5c6d35435f
dwarowski <realcarenthusiast@yandex.ru>
2025-03-09 18:07:20 +0300

    topic imported in

--- src/tests/tests.module.ts
+++ src/tests/tests.module.ts
@@ -7,11 +7,13 @@ import { TestsService } from './tests.service';
 import { TestsEntity } from './entity/test.entity';
 import { RatingService } from 'src/ratings/rating.service';
 import { RatingEntity } from 'src/ratings/entity/rating.entity';
+import { TopicEntity } from 'src/topics/entity/topic.entity';
+import { TopicsService } from 'src/topics/topics.service';
 
 
 @Module({
-  imports: [ConfigModule, TypeOrmModule.forFeature([TestsEntity, RatingEntity])],
+  imports: [ConfigModule, TypeOrmModule.forFeature([TestsEntity, RatingEntity, TopicEntity])],
   controllers: [TestsController],
-  providers: [TestsService, RatingService]
+  providers: [TestsService, RatingService, TopicsService]
 })
 export class TestsModule {}
