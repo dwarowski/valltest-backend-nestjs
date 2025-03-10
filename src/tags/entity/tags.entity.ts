07d2fad7c53f6100ca838e0f850e108e2b2515b5
dwarowski <realcarenthusiast@yandex.ru>
2025-03-10 15:17:54 +0300

    removed connections

--- src/tags/entity/tags.entity.ts
+++ src/tags/entity/tags.entity.ts
@@ -1,24 +1,14 @@
-import { ApiHideProperty } from '@nestjs/swagger';
-
 import {
     Column,
     Entity,
     PrimaryGeneratedColumn,
-    ManyToOne
 } from 'typeorm';
-
-import { TestsEntity } from 'src/tests/entity/test.entity';
 
 @Entity('tags')
 export class TagsEntity {
-    
-    @ApiHideProperty()
     @PrimaryGeneratedColumn()
     id: number;
 
-    @ManyToOne(() => TestsEntity, (test) => test.id)
-    test: TestsEntity
-    
     @Column()
     tag: string
 
