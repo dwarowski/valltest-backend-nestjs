477650b46460fe96bf954309e9aff22e8e67f500
dwarowski <realcarenthusiast@yandex.ru>
2025-03-11 14:33:38 +0300

    created test-tag service and relation func

---
+++ src/test-tag/test-tag.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TestTagEntity } from './entity/test-tag.entity';
+import { Repository } from 'typeorm';
+import { TestsEntity } from 'src/tests/entity/test.entity';
+import { TagsEntity } from 'src/tags/entity/tags.entity';
+
+@Injectable()
+export class TestTagService {
+    constructor(
+        @InjectRepository(TestTagEntity)
+        private repository: Repository<TestTagEntity>,
+    ) { }
+
+    async createRelationTestTag(test: TestsEntity, tag: TagsEntity){
+        return await this.repository.save(
+            {
+                test: test,
+                tag: tag
+            }
+        )
+
+    }
+}
