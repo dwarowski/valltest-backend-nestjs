a1c6c2f4b73c143b41bdea29d7b3647d1b84299c
dwarowski <realcarenthusiast@yandex.ru>
2025-03-14 20:29:53 +0300

    import user

--- src/test-favorites/test-favorites.module.ts
+++ src/test-favorites/test-favorites.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TestFavoritesController } from './test-favorites.controller';
 import { TestFavoritesService } from './test-favorites.service';
 import { FavoriteTestEntity } from './entity/favorite-test.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
-  imports: [ConfigModule, TypeOrmModule.forFeature([FavoriteTestEntity])],
+  imports: [ConfigModule, TypeOrmModule.forFeature([FavoriteTestEntity, User])],
   controllers: [TestFavoritesController],
   providers: [TestFavoritesService]
 })
