b822ea110140155a210fb5ea4448db800ae091df
dwarowski <realcarenthusiast@yandex.ru>
2025-03-20 15:25:53 +0300

    service and contoller added

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { RolesService } from './roles.service';
+import { RolesController } from './roles.controller';
 
-@Module({})
+@Module({
+  providers: [RolesService],
+  controllers: [RolesController]
+})
 export class RolesModule {}
