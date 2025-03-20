cb90fe7c39cd6f982cea137186e541b3c50a6190
dwarowski <realcarenthusiast@yandex.ru>
2025-03-20 15:48:33 +0300

    entity added

--- src/roles-users/roles-users.module.ts
+++ src/roles-users/roles-users.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RolesUsersService } from './roles-users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RolesUsersEntity } from './entity/roles-users.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RolesUsersEntity])],
   providers: [RolesUsersService]
 })
 export class RolesUsersModule {}
