922396aa462d1cca5cca4b823eadfe09f5308fd5
dwarowski <realcarenthusiast@yandex.ru>
2025-03-20 15:48:16 +0300

    added relation to user id

--- src/roles-users/entity/roles-users.entity.ts
+++ src/roles-users/entity/roles-users.entity.ts
@@ -1,11 +1,15 @@
 import { RoleEntity } from "src/roles/entity/role.entity";
+import { User } from "src/user/entities/user.entity";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('role-user')
-export class RolesUsersEntity { 
+export class RolesUsersEntity {
     @PrimaryGeneratedColumn()
     id: number;
 
     @ManyToOne(() => RoleEntity, (role) => role.id, { onDelete: 'CASCADE' })
     role: RoleEntity;
+
+    @ManyToOne(() => User, (user) => user.id, { onDelete: 'CASCADE' })
+    user: User;
 }
