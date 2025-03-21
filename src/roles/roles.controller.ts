7ac1f7b55503d01a6ebb48ada0441c144eec2079
dwarowski <realcarenthusiast@yandex.ru>
2025-03-21 12:37:29 +0300

    CRD endpoints

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { RolesService } from './roles.service';
+import { createRoleDto } from './dto/create-role.dto';
 
 @Controller('roles')
-export class RolesController {}
+export class RolesController {
+    constructor(private readonly rolesService: RolesService) { }
+
+    @Get(':name')
+    getRole(@Param('name') name: string) {
+        return this.rolesService.getRole(name)
+    }
+
+    @Post()
+    createRole(@Body() dto: createRoleDto ) {
+        return this.rolesService.createRole(dto)
+    }
+
+    @Delete(':name')
+    deleteRole(@Param('name') name: string) {
+        return this.rolesService.deleteRole(name)
+    }
+
+
+}
