4e697b3b67570495386d4234c159b89b834961e4
dwarowski <realcarenthusiast@yandex.ru>
2025-03-21 12:37:19 +0300

    CRD func

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RoleEntity } from './entity/role.entity';
+import { Repository } from 'typeorm';
+import { createRoleDto } from './dto/create-role.dto';
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+    constructor(
+        @InjectRepository(RoleEntity)
+        private repository: Repository<RoleEntity>,
+    ) { }
+
+
+    async getRole(name: string) {
+        return await this.repository.findBy({role: name})
+    } 
+
+    async createRole(dto: createRoleDto) {
+        return await this.repository.save(dto)
+
+    }
+    async deleteRole(name: string) {
+        return await this.repository.delete({role: name})
+    }
+}
