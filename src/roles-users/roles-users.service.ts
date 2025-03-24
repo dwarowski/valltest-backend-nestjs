9b7abe891c0e4589c8fbda046c8b7e8793fe19e3
dwarowski <realcarenthusiast@yandex.ru>
2025-03-25 01:50:39 +0300

    dto input instead and deletion

--- src/roles-users/roles-users.service.ts
+++ src/roles-users/roles-users.service.ts
@@ -4,6 +4,8 @@ import { RolesUsersEntity } from './entity/roles-users.entity';
 import { Repository } from 'typeorm';
 import { RolesService } from 'src/roles/roles.service';
 import { UserService } from 'src/user/user.service';
+import { assignRoleDto } from './dto/assign-role.dto';
+import { removeRelationDto } from './dto/remove-relation.dto';
 
 @Injectable()
 export class RolesUsersService {
@@ -16,8 +18,9 @@ export class RolesUsersService {
         private repository: Repository<RolesUsersEntity>,
     ) { }
 
-    async addRoleToUser(roleName: string, user: string) {
-        const roleEntity = await this.rolesService.getRole(roleName);
+    async addRoleToUser(dto: assignRoleDto) {
+        const { role, user } = dto
+        const roleEntity = await this.rolesService.getRole(role);
 
         if (!roleEntity) {
             throw new BadRequestException('role doesn`t exist')
@@ -26,5 +29,27 @@ export class RolesUsersService {
         const userEntity = await this.userService.getUserByName(user)
 
         return await this.repository.save({ user: userEntity, role: roleEntity })
+    }
+
+    async deleteRoleRelation(dto: removeRelationDto) {
+        const {user, role } = dto 
+
+        const roleRelation = await this.repository.findOne({
+            where: {
+                user: {
+                    username: user
+                },
+                role: {
+                    role: role
+                }
+            },
+            relations: ['user', 'role']
+        })
+
+        if (!roleRelation) {
+            throw new BadRequestException('relation doesn`t exist')
+        }
+
+        return this.repository.delete(roleRelation)
     }
 }
