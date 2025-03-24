323fe139934b9a638686b2ec1de52597a4b7a592
dwarowski <realcarenthusiast@yandex.ru>
2025-03-25 00:17:34 +0300

    import roles and users

--- src/roles-users/roles-users.module.ts
+++ src/roles-users/roles-users.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { RolesUsersService } from './roles-users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RolesUsersEntity } from './entity/roles-users.entity';
+import { RolesModule } from 'src/roles/roles.module';
+import { UserModule } from 'src/user/user.module';
+import { RolesUsersController } from './roles-users.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RolesUsersEntity])],
-  providers: [RolesUsersService]
+  imports: [RolesModule, UserModule, TypeOrmModule.forFeature([RolesUsersEntity])],
+  providers: [RolesUsersService],
+  exports: [RolesUsersService],
+  controllers: [RolesUsersController]
 })
 export class RolesUsersModule {}
