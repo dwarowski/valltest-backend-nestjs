a799ada48e277eb346abf0b9a6cd25cb1bb05cbd
dwarowski <realcarenthusiast@yandex.ru>
2025-03-27 00:28:18 +0300

    optimized token add to cookie
    new func to add token to cookie response and message to return
    Register also adds to cookie now

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,14 +9,24 @@ export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
   @Post('register')
-  async register(@Body() registerDto: RegisterDto) {
-    return this.authService.register(registerDto);
+  async register(@Body() registerDto: RegisterDto, @Res({ passthrough: true }) res: Response) {
+    const { access_token } = await this.authService.register(registerDto);
+    return this.addTokenToCookie(access_token, res, 'register successful')
   }
 
   @Post('login')
   async login(@Body() loginDto: LoginDto, @Res({ passthrough: true }) res: Response) {
     const { access_token } = await this.authService.login(loginDto);
+    return this.addTokenToCookie(access_token, res, 'login successful')
+  }
 
+  @Post('logout')
+  async logout(@Res({ passthrough: true }) res: Response) {
+    res.clearCookie('access_token')
+    return { message: 'Logout successful' }
+  }
+
+  private addTokenToCookie(access_token: string, res: Response, message: string) {
     if (!access_token) {
       throw new UnauthorizedException()
     }
@@ -27,13 +37,6 @@ export class AuthController {
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
 
-    return { message: 'login successful' }
+    return { message: message }
   }
-
-  @Post('logout')
-  async logout(@Res({ passthrough: true }) res: Response) {
-    res.clearCookie('access_token')
-    return { message: 'Logout successful' }
-  }
-
 }
