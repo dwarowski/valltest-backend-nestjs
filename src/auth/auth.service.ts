1814360538006586b95421d4af93035efc14645d
dwarowski <realcarenthusiast@yandex.ru>
2025-03-26 22:49:17 +0300

    returns jwt not user entity

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,17 +9,19 @@ import { User } from '../user/entities/user.entity';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
+    private readonly jwtService: JwtService,
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
   ) { }
 
   async register(
     registerDto: RegisterDto,
-  ): Promise<Omit<User, 'hashed_password'>> {
+  ): Promise<{ access_token: string }> {
     // Проверяем, что пользователь с таким email уже не существует
     const existingUser = await this.userRepository.findOne({
       where: { email: registerDto.email },
@@ -42,15 +44,16 @@ export class AuthService {
     // Сохраняем пользователя в базе данных
     const savedUser = await this.userRepository.save(user);
 
-    // Убираем пароль из ответа
-    const { hashed_password, ...userWithoutPassword } = savedUser;
-    return userWithoutPassword;
+    const loginDto: LoginDto = { email: registerDto.email, password: registerDto.password };
+
+    return this.login(loginDto);
   }
 
-  async login(loginDto: LoginDto): Promise<Omit<User, 'hashed_password'>> {
+  async login(loginDto: LoginDto): Promise<{ access_token: string }> {
     // Ищем пользователя по email
     const user = await this.userRepository.findOne({
       where: { email: loginDto.email },
+      relations: ['role', 'role.role']
     });
     if (!user) {
       throw new UnauthorizedException('Неверный email или пароль');
@@ -64,9 +67,15 @@ export class AuthService {
     if (!isPasswordValid) {
       throw new UnauthorizedException('Неверный email или пароль');
     }
-
     // Убираем пароль из ответа
     const { hashed_password, ...userWithoutPassword } = user;
-    return userWithoutPassword;
+
+    return {
+      access_token: await this.jwtService.signAsync(userWithoutPassword, {
+        secret: process.env.JWT_SECRET,
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      })
+    }
+
   }
 }
