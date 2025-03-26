a8a408f9f0b16e97376f337b487fe6f31c90990b
dwarowski <realcarenthusiast@yandex.ru>
2025-03-26 10:22:34 +0300

    basic role validation

---
+++ src/guards/roles.guard.ts
@@ -0,0 +1,33 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { ROLES_KEY } from "src/decorators/roles-decorator";
+import { RolesUsersService } from "src/roles-users/roles-users.service";
+
+@Injectable()
+export class RolesGuards implements CanActivate {
+    constructor(
+        private reflector: Reflector,
+        private RolesUserService: RolesUsersService
+    ) { }
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ])
+        
+        if (!requiredRoles) {
+            return true
+        }
+
+        const { user } = context.switchToHttp().getRequest();
+        
+        if (!user || !user.id) {
+            return false;
+        }
+
+        const userRoles = await this.RolesUserService.getUserRoles(user.name)
+
+        return requiredRoles.some(role => userRoles.includes(role));
+    }
+}
