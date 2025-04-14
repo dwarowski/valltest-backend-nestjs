ebeb6ab10d89e6d5daa40194c635ee79fdd6afeb
dwarowski <realcarenthusiast@yandex.ru>
2025-04-14 21:12:16 +0300

    new prefix for api

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('dstu');
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
 
   // Включаем CORS
