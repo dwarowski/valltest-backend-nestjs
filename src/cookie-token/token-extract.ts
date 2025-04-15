0c1123f47c839514820dfe574cc0f19b52019cba
dwarowski <realcarenthusiast@yandex.ru>
2025-04-15 12:32:45 +0300

    fixed jwtSerivce injection

--- src/cookie-token/token-extract.ts
+++ src/cookie-token/token-extract.ts
@@ -1,19 +1,21 @@
 import { UnauthorizedException } from "@nestjs/common";
 import { Request } from "express";
 import { tokenPayload } from "./token-payload";
+import { JwtService } from "@nestjs/jwt";
 
 export async function extractTokenFromCookie(req: Request): Promise<tokenPayload>{
-    const token = req.cookies['access_token'];
-    if (!token) {
-      throw new UnauthorizedException('No access token found.');
-    }
+  const jwtService = new JwtService();
+  const token = req.cookies['access_token'];
+  if (!token) {
+    throw new UnauthorizedException('No access token found.');
+  }
 
-    try {
-      const payload = await this.jwtService.verify(token, {
-        secret: process.env.JWT_SECRET,
-      });
-      return payload;
-    } catch (error) {
-      throw new UnauthorizedException('Invalid or expired access token.');
-    }
+  try {
+    const payload = await jwtService.verifyAsync(token, {
+      secret: process.env.JWT_SECRET,
+    });
+    return payload;
+  } catch (error) {
+    throw new UnauthorizedException('Invalid or expired access token.');
+  }
 }
