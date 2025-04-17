ec734657e676140adb58f9b108c4c65f358e02ef
dwarowski <realcarenthusiast@yandex.ru>
2025-04-17 23:54:51 +0300

    Update get-user-tests.dto.ts

--- src/tests/dto/get-user-tests.dto.ts
+++ src/tests/dto/get-user-tests.dto.ts
@@ -1,7 +1,10 @@
+import { TagsEntity } from "src/tags/entity/tags.entity";
+
 export class UserTests {
     id: number;
     testName: string;
     difficulty: number;
     createdAt: Date;
     averageRating: number;
+    testTag: TagsEntity[];
 }
