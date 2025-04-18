d83068266fa6ef93bb1b89ce03c3c4ad0f735486
dwarowski <realcarenthusiast@yandex.ru>
2025-04-18 13:19:52 +0300

    answer relation added

--- src/problems/entity/problems.entity.ts
+++ src/problems/entity/problems.entity.ts
@@ -1,7 +1,8 @@
 import { ApiHideProperty } from '@nestjs/swagger';
-import { Column, Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne, OneToMany } from 'typeorm';
 
 import { TestsEntity } from 'src/tests/entity/test.entity';
+import { AnswersEntity } from 'src/answers/entity/answers.entity';
 
 @Entity('problems')
 export class ProblemsEntity {
@@ -14,4 +15,8 @@ export class ProblemsEntity {
 
   @Column()
   question: string;
+
+  @OneToMany(() => AnswersEntity, (answer) => answer.problem, { onDelete: 'CASCADE' })
+  answers: AnswersEntity[];
+
 }
