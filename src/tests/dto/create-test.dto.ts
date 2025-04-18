ae019ec5f01d7df02aaea2ddcdf9ae256aca1e85
dwarowski <realcarenthusiast@yandex.ru>
2025-04-18 14:23:27 +0300

    Update create-test.dto.ts

--- src/tests/dto/create-test.dto.ts
+++ src/tests/dto/create-test.dto.ts
@@ -1,10 +1,13 @@
 import { IsString } from 'class-validator';
+import { CreateAnswerDto } from 'src/answers/dto/create-answer.dto';
 
 export class CreateTestDto {
   difficulty: number;
   @IsString()
   testName: string;
-  timeForTest: number;
-  userAuthorId: string;
-  topicId: number;
+  topicName: string;
+  questions: {
+    question: string;
+    answers: CreateAnswerDto[]
+  }[]
 }
