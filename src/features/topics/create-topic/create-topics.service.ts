d29d99151e5bb7233518ab2e9eed17bdff0188c2
dwarowski <realcarenthusiast@yandex.ru>
2025-04-19 01:36:49 +0300

    new exception added

--- src/features/topics/create-topic/create-topics.service.ts
+++ src/features/topics/create-topic/create-topics.service.ts
@@ -1,10 +1,11 @@
-import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { ForbiddenException, Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { CreateTopicDto } from './create-topic.dto';
 import { TopicEntity } from '../../../entities/topics/topic.entity';
 import { GetSubjectService } from 'src/features/subjects/get-subject/get-subject.service';
+import { GetTopicService } from '../get-topic/get-topics.service';
 
 @Injectable()
 export class CreateTopicService {
@@ -13,17 +14,23 @@ export class CreateTopicService {
     private readonly topicRepository: Repository<TopicEntity>,
     @Inject(GetSubjectService)
     private readonly getSubjectService: GetSubjectService,
+    @Inject(GetTopicService)
+    private readonly getTopicService: GetTopicService,
   ) {}
 
   // Создание темы
-  async create(createTopicDto: CreateTopicDto): Promise<TopicEntity> {
+  async create(createTopicDto: CreateTopicDto) {
     const subject = await this.getSubjectService.execute(createTopicDto.subjectName)
+    const topicEntity = await this.getTopicService.getTopicByName(createTopicDto.topicName)
+    if (topicEntity) {
+      throw new ForbiddenException('Topic already exsist')
+    }
 
-    const topic = this.topicRepository.create({
+    const topic = await this.topicRepository.save({
       topicName: createTopicDto.topicName,
       subject,
     });
 
-    return this.topicRepository.save(topic);
+    return topic;
   }
 }
