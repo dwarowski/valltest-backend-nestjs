67b8a8ed0961ab7b90202b26173305402726e116
dwarowski <realcarenthusiast@yandex.ru>
2025-04-18 22:33:53 +0300

    cleared user-answers

--- src/routes/users-answers/user-answers.module.ts
+++ src/routes/users-answers/user-answers.module.ts
@@ -3,12 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserAnswersEntity } from '../../entities/user-answers/user-answer.entity';
-import { UserAnswersController } from './user-answers.controller';
-import { UserAnswersService } from '../../features/users-answers/user-answers.service';
 
 @Module({
   imports: [ConfigModule, TypeOrmModule.forFeature([UserAnswersEntity])],
-  controllers: [UserAnswersController],
-  providers: [UserAnswersService],
+  controllers: [],
+  providers: [],
 })
 export class UserAnswersModule {}
