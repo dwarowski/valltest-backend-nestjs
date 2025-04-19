2f2d4f861da02dcf2e0d954769579680aba78adf
dwarowski <realcarenthusiast@yandex.ru>
2025-04-20 00:47:18 +0300

    refactored: relation already exists

--- src/features/ratings/get-test-ratings/get-test-rating.service.ts
+++ src/features/ratings/get-test-ratings/get-test-rating.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { RatingEntity } from '../../../entities/ratings/rating.entity';
- 
+
 @Injectable()
 export class GetTestRatingService {
   constructor(
@@ -14,13 +14,12 @@ export class GetTestRatingService {
   // Получить все оценки для теста
   async getRatingsByTest(testId: number) {
     const testRatings = await this.ratingRepository.find({
-      where: { test: { id: testId } },
-      relations: ['user'], // Подгружаем информацию о пользователе
+      where: { test: { id: testId } }
     });
-    
+
     const cleanTestRatings = await Promise.all(testRatings.map(rating => {
-      const {id, user, ...cleanTestRating} = rating
-      return {...cleanTestRating, user: { username: user.username, avatar_location: user.avatar_location}}
+      const { id, user, ...cleanTestRating } = rating
+      return { ...cleanTestRating, user: { username: user.username, avatar_location: user.avatar_location } }
     }));
 
     return cleanTestRatings
