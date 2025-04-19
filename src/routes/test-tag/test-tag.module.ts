329a58a7bcbf479dc633de76e75dd140f0372981
dwarowski <realcarenthusiast@yandex.ru>
2025-04-20 01:11:23 +0300

    deleted: unused service

--- src/routes/test-tag/test-tag.module.ts
+++ src/routes/test-tag/test-tag.module.ts
@@ -5,11 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TestTagEntity } from '../../entities/test-tag/test-tag.entity';
 import { CreateRelationTestTagService } from 'src/features/test-tag/create-relation/create-relation-test-tag.service';
 import { DeleteRelationTestTagService } from 'src/features/test-tag/delete-relation/delete-relation-test-tag.service';
-import { GetTagByNameService } from 'src/features/test-tag/get-tag-by-name/get-tag-by-name.service';
 
 @Module({
   imports: [ConfigModule, TypeOrmModule.forFeature([TestTagEntity])],
-  providers: [CreateRelationTestTagService, DeleteRelationTestTagService, GetTagByNameService],
-  exports: [GetTagByNameService, CreateRelationTestTagService, DeleteRelationTestTagService],
+  providers: [CreateRelationTestTagService, DeleteRelationTestTagService],
+  exports: [CreateRelationTestTagService, DeleteRelationTestTagService],
 })
 export class TestTagModule {}
