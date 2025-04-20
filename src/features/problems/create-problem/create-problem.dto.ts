bf16ca72cb418e6fdc0edeeecb686303f9a5227d
dwarowski <realcarenthusiast@yandex.ru>
2025-04-20 18:33:29 +0300

    add: answers and test id

--- src/features/problems/create-problem/create-problem.dto.ts
+++ src/features/problems/create-problem/create-problem.dto.ts
@@ -1,6 +1,13 @@
-import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
+import { CreateAnswerDto } from 'src/features/answers/create-answer/create-answer.dto';
 
 export class CreateProblemDto {
+  @ApiProperty({readOnly: true})
+  @IsNumber()
+  test: number
   @IsString()
   question: string;
+
+  answers: CreateAnswerDto[]
 }
