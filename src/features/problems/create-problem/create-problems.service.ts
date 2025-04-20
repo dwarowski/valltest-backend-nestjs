007ea09c1b0f41a46ce5e8ac2982ca5a71f98107
dwarowski <realcarenthusiast@yandex.ru>
2025-04-20 18:38:31 +0300

    new: creating answers with problem

--- src/features/problems/create-problem/create-problems.service.ts
+++ src/features/problems/create-problem/create-problems.service.ts
@@ -1,17 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { CreateProblemDto } from './create-problem.dto';
 import { ProblemsEntity } from '../../../entities/problems/problems.entity';
+import { CreateAnswerService } from 'src/features/answers/create-answer/create-answers.service';
 
 @Injectable()
-export class CreateProblemsService {
+export class CreateProblemService {
   constructor(
+    @Inject(CreateAnswerService)
+    private readonly createAnswer: CreateAnswerService,
     @InjectRepository(ProblemsEntity)
     private readonly repository: Repository<ProblemsEntity>,
   ) {}
-  async createProblem(testId: number, dto: CreateProblemDto) {
-    return await this.repository.save({ ...dto, test: testId });
+
+  async execute(dto: CreateProblemDto) {
+    const problemEntity = await this.repository.save(dto);
+
+    const answerEntities = await Promise.all(
+      problemEntity.answers.map(async (answer) => {
+        answer.problem = problemEntity.id
+        const createdAnswer = await this.createAnswer.execute(answer);
+        return createdAnswer;
+      }),
+    );
+    return { question: problemEntity.question, answers: answerEntities }
   }
 }
