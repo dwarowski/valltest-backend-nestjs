5dd3abb581cd6f14a58400b3273ae4fd1fd7fbb2
dwarowski <realcarenthusiast@yandex.ru>
2025-04-20 18:29:06 +0300

    add: problem id

--- src/features/answers/create-answer/create-answer.dto.ts
+++ src/features/answers/create-answer/create-answer.dto.ts
@@ -1,6 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsString } from 'class-validator';
 
 export class CreateAnswerDto {
+  @ApiProperty({ readOnly: true })
+  @IsString()
+  problem: number;
   @IsString()
   value: string;
   @IsBoolean()
