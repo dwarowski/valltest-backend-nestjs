a8de7d7a73b647730d9ad05944d7a3ea1cf9d2cc
dwarowski <realcarenthusiast@yandex.ru>
2025-04-21 18:42:12 +0300

    add: service controller and test module  import

--- src/routes/users-answers/user-answers.module.ts
+++ src/routes/users-answers/user-answers.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserAnswersEntity } from '../../entities/user-answers/user-answer.entity';
+import { UserAnswersController } from './user-answers.controller';
+import { TestsModule } from '../tests/tests.module';
+import { CheckAnswersService } from 'src/features/user-answers/check-user-answers/check-answers.serivce';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserAnswersEntity])],
-  controllers: [],
-  providers: [],
+  imports: [TestsModule ,TypeOrmModule.forFeature([UserAnswersEntity])],
+  controllers: [UserAnswersController],
+  providers: [CheckAnswersService],
 })
 export class UserAnswersModule {}
