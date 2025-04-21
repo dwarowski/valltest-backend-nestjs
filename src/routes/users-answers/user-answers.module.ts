6e95ca5e43ff6fff0ea7b05d41de4c4e16281353
dwarowski <realcarenthusiast@yandex.ru>
2025-04-22 01:54:32 +0300

    add: initialzed new services

--- src/routes/users-answers/user-answers.module.ts
+++ src/routes/users-answers/user-answers.module.ts
@@ -5,10 +5,12 @@ import { UserAnswersEntity } from '../../entities/user-answers/user-answer.entit
 import { UserAnswersController } from './user-answers.controller';
 import { TestsModule } from '../tests/tests.module';
 import { CheckAnswersService } from 'src/features/user-answers/check-user-answers/check-answers.serivce';
+import { GetUserTestAnsweresService } from 'src/features/user-answers/get-user-test-answers/get-user-test-answers.service';
+import { SaveUserAnswersService } from 'src/features/user-answers/save-user-answers/save-user-answers.service';
 
 @Module({
   imports: [TestsModule ,TypeOrmModule.forFeature([UserAnswersEntity])],
   controllers: [UserAnswersController],
-  providers: [CheckAnswersService],
+  providers: [CheckAnswersService, GetUserTestAnsweresService, SaveUserAnswersService],
 })
 export class UserAnswersModule {}
