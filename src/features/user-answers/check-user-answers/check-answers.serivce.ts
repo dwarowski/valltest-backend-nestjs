3630236a3879cfc513917b202d75a71078c21cd3
dwarowski <realcarenthusiast@yandex.ru>
2025-04-23 01:26:17 +0300

    create: check user answers and return result

---
+++ src/features/user-answers/check-user-answers/check-answers.serivce.ts
@@ -0,0 +1,39 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { Request } from "express";
+import { GetTestCorrectAnswersService } from "src/features/tests/get-test-correct-answers/get-test-corect-answers.service";
+import { extractTokenFromCookie } from "src/shared/utils/functions/extract-token-from-cookie/token-extract";
+import { GetUserTestAnsweresService } from "../get-user-test-answers/get-user-test-answers.service";
+import { UserAnswersDto } from "../save-user-answers/user-answers.dto";
+import { CorrectAnswersDto } from "./correct-answers.dto";
+
+@Injectable()
+export class CheckAnswersService {
+    constructor(
+        @Inject(GetTestCorrectAnswersService)
+        private readonly getTestCorrectAnswers: GetTestCorrectAnswersService,
+        @Inject(GetUserTestAnsweresService)
+        private readonly getUserTestAnswers: GetUserTestAnsweresService
+    ) { }
+
+    async execute(req: Request, testId: number) {
+        const payload = await extractTokenFromCookie(req)
+        const userId = payload.id
+        const correctAnswers = await this.getTestCorrectAnswers.execute(testId)
+        const userAnswers = await this.getUserTestAnswers.execute(userId)
+        const userResult = await this.compareAnswers(userAnswers, correctAnswers)
+        return userResult
+    }
+
+    private async compareAnswers(userAnswers: UserAnswersDto, correctAnswers: CorrectAnswersDto) {
+        let correctCount = 0;
+        for (const correct of correctAnswers.correctAnswers) {
+            for (const user of userAnswers.userAnswers) {
+                if (correct.problemId === user.problemId && correct.answerId === user.answerId) {
+                    correctCount++;
+                    break;
+                }
+            }
+        }
+        return correctCount
+    }
+}
