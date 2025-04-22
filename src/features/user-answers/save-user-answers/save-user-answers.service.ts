17c80eea1111f915f238b4a779feb2d9e0702d8a
dwarowski <realcarenthusiast@yandex.ru>
2025-04-23 01:23:30 +0300

    create: save user answers for test

---
+++ src/features/user-answers/save-user-answers/save-user-answers.service.ts
@@ -0,0 +1,54 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { Request } from "express";
+import { UserAnswersDto } from "./user-answers.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { UserAnswersEntity } from "src/entities/user-answers/user-answer.entity";
+import { Repository } from "typeorm";
+import { extractTokenFromCookie } from "src/shared/utils/functions/extract-token-from-cookie/token-extract";
+import { GetTestsIdService } from "src/features/tests/get-test-id/get-tests-id.service";
+import { GetUserService } from "src/features/users/get-user/get-user.service";
+import { GetProblemService } from "src/features/problems/get-problem/get-problem.service";
+import { GetAnswerService } from "src/features/answers/get-answer/get-answer.service";
+
+@Injectable()
+export class SaveUserAnswersService {
+    constructor(
+        @InjectRepository(UserAnswersEntity)
+        private readonly userAnswersRepository: Repository<UserAnswersEntity>,
+        @Inject(GetTestsIdService)
+        private readonly getTest: GetTestsIdService,
+        @Inject(GetUserService)
+        private readonly getUser: GetUserService,
+        @Inject(GetProblemService)
+        private readonly getProblem: GetProblemService,
+        @Inject(GetAnswerService)
+        private readonly getAnswer: GetAnswerService,
+    ) { }
+
+    async execute(req: Request, dto: UserAnswersDto) {
+        const { testId, userAnswers } = dto
+        const payload = await extractTokenFromCookie(req)
+        const userId = payload.id
+
+        const userEntity = await this.getUser.execute(userId, 'id')
+        const testsEntity = await this.getTest.execute(testId, 'entity')
+
+        await Promise.all(userAnswers.map(async (userAnswer) => {
+            try {
+                const problemEntity = await this.getProblem.execute(userAnswer.problemId)
+                const answerEntity = await this.getAnswer.execute(userAnswer.answerId)
+
+                await this.userAnswersRepository.save({
+                    user: userEntity,
+                    test: testsEntity,
+                    problem: problemEntity,
+                    answer: answerEntity
+                })
+            }
+            catch (error) {
+                throw error
+            }
+        }))
+        return 'answers saved'
+    }
+}
