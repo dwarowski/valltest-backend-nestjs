328dbd9515c3a928a12fcb00ab5ae241db5d0d57
dwarowski <realcarenthusiast@yandex.ru>
2025-04-23 10:56:04 +0300

    fix: return only by test id not every answer

--- src/features/user-answers/get-user-test-answers/get-user-test-answers.service.ts
+++ src/features/user-answers/get-user-test-answers/get-user-test-answers.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { UserAnswersEntity } from "src/entities/user-answers/user-answer.entity";
 import { Repository } from "typeorm";
@@ -8,13 +8,18 @@ export class GetUserTestAnsweresService {
     constructor(
         @InjectRepository(UserAnswersEntity)
         private readonly userAnswersRepository: Repository<UserAnswersEntity>
-    ) { }
+    ) {}
 
-    async execute(userId: string) {
+    async execute(userId: string, testId: number) {
         const userAnswers = await this.userAnswersRepository.find({
-            where: { user: { id: userId } },
+            where: { user: { id: userId }, test: {id: testId}},
             relations: ['test', 'user', 'problem', 'answer'],
         });
+
+        if (userAnswers.length == 0) {
+            throw new NotFoundException('User dont have answers')
+        }
+
         const userAnswersFilterd = await Promise.all(userAnswers.map(async (userAnswer) => {
             const { problem, answer } = userAnswer
             return { problemId: problem.id, answerId: answer.id }
