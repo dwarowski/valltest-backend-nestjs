a7777755ce4e57127adf7b51fc34d9782f38deee
dwarowski <realcarenthusiast@yandex.ru>
2025-04-24 01:09:18 +0300

    refactor: return type defined

--- src/features/tests/get-test-user/get-user-tests.service.ts
+++ src/features/tests/get-test-user/get-user-tests.service.ts
@@ -7,6 +7,7 @@ import { extractTokenFromCookie } from 'src/shared/utils/functions/extract-token
 
 import { TestsEntity } from '../../../entities/tests/test.entity';
 import { GetTestAverageRatingService } from 'src/features/ratings/get-test-average-rating/get-test-average-rating.service';
+import { TestWithRatingDto } from 'src/shared/utils/dto/test-with-rating/test-with-rating.dto';
 
 @Injectable()
 export class GetUsersTestsService {
@@ -17,7 +18,7 @@ export class GetUsersTestsService {
     private readonly testsRepository: Repository<TestsEntity>,
   ) { }
 
-  async execute(req: Request): Promise<any> {
+  async execute(req: Request): Promise<TestWithRatingDto[]> {
     const payload = await extractTokenFromCookie(req);
     const userId = payload.id;
 
