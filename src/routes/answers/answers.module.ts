c0fb00cc0460f7bdc31bf5fbe4b98046e3a3de86
dwarowski <realcarenthusiast@yandex.ru>
2025-04-25 01:45:19 +0300

    problem module import

--- src/routes/answers/answers.module.ts
+++ src/routes/answers/answers.module.ts
@@ -1,14 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AnswersEntity } from '../../entities/answers/answers.entity';
 import { CreateAnswerService } from 'src/features/answers/create-answer/create-answers.service';
 import { GetAnswerService } from 'src/features/answers/get-answer/get-answer.service';
+import { ProblemsModule } from '../problems/problems.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AnswersEntity])],
+  imports: [ProblemsModule, TypeOrmModule.forFeature([AnswersEntity])],
   controllers: [],
   providers: [CreateAnswerService, GetAnswerService],
   exports: [CreateAnswerService, GetAnswerService],
 })
-export class AnswersModule {}
+export class AnswersModule { }
