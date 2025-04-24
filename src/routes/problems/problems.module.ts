8eb108ddb720fa1d76c8336bda0be422ab0196c1
dwarowski <realcarenthusiast@yandex.ru>
2025-04-25 01:29:09 +0300

    fix: imported important service and entity

--- src/routes/problems/problems.module.ts
+++ src/routes/problems/problems.module.ts
@@ -1,15 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ProblemsEntity } from '../../entities/problems/problems.entity';
 import { CreateProblemService } from '../../features/problems/create-problem/create-problems.service';
 import { AnswersModule } from '../answers/answers.module';
 import { GetProblemService } from 'src/features/problems/get-problem/get-problem.service';
+import { GetTestsIdService } from 'src/features/tests/get-test-id/get-tests-id.service';
+import { TestsEntity } from 'src/entities/tests/test.entity';
 
 @Module({
-  imports: [AnswersModule, TypeOrmModule.forFeature([ProblemsEntity])],
+  imports: [AnswersModule, TypeOrmModule.forFeature([ProblemsEntity, TestsEntity])], //TODO circular import if test module imported so using provider and entity
   controllers: [],
-  providers: [CreateProblemService, GetProblemService],
+  providers: [CreateProblemService, GetProblemService, GetTestsIdService],
   exports: [CreateProblemService, GetProblemService],
 })
 export class ProblemsModule {}
