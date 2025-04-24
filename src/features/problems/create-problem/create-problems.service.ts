27ee961f0e72cf0e1c868df877e14d21a775f907
dwarowski <realcarenthusiast@yandex.ru>
2025-04-25 01:28:14 +0300

    fix: geting test entity instead just id

--- src/features/problems/create-problem/create-problems.service.ts
+++ src/features/problems/create-problem/create-problems.service.ts
@@ -5,18 +5,23 @@ import { Repository } from 'typeorm';
 import { CreateProblemDto } from './create-problem.dto';
 import { ProblemsEntity } from '../../../entities/problems/problems.entity';
 import { CreateAnswerService } from 'src/features/answers/create-answer/create-answers.service';
+import { GetTestsIdService } from 'src/features/tests/get-test-id/get-tests-id.service';
 
 @Injectable()
 export class CreateProblemService {
   constructor(
     @Inject(CreateAnswerService)
     private readonly createAnswer: CreateAnswerService,
+    @Inject(GetTestsIdService)
+    private readonly getTest: GetTestsIdService,
     @InjectRepository(ProblemsEntity)
     private readonly problemsRepository: Repository<ProblemsEntity>,
   ) {}
 
   async execute(dto: CreateProblemDto) {
-    const problemEntity = await this.problemsRepository.save(dto);
+    const testEntity = await this.getTest.execute(dto.testId, 'entity');
+
+    const problemEntity = await this.problemsRepository.save({ test: testEntity, ...dto });
 
     const answerEntities = await Promise.all(
       problemEntity.answers.map(async (answer) => {
