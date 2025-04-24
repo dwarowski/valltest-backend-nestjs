d367adff968fd6cc809545c366ac43b9ace2f678
dwarowski <realcarenthusiast@yandex.ru>
2025-04-25 01:29:51 +0300

    refactor: removed unused import

--- src/routes/problems/problems.module.ts
+++ src/routes/problems/problems.module.ts
@@ -1,4 +1,4 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ProblemsEntity } from '../../entities/problems/problems.entity';
