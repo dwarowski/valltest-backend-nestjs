b9704d3954dc1f873e902384d02124bb34fddc0e
dwarowski <realcarenthusiast@yandex.ru>
2025-04-26 21:47:07 +0300

    add: normalization

--- src/features/topics/create-topic/create-topic.dto.ts
+++ src/features/topics/create-topic/create-topic.dto.ts
@@ -1,11 +1,14 @@
+import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateTopicDto {
   @IsNotEmpty()
   @IsString()
+  @Transform(({ value }) => value.trim().toLowerCase())
   topicName: string;
 
   @IsNotEmpty()
   @IsString()
+  @Transform(({ value }) => value.trim().toLowerCase())
   subjectName: string;
 }
