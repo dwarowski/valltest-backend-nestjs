f8b8827833a4d485257867bbb12bf7c2508934f3
dwarowski <realcarenthusiast@yandex.ru>
2025-04-26 21:05:06 +0300

    add: subject name normflization

--- src/features/subjects/create-subject/create-subject.dto.ts
+++ src/features/subjects/create-subject/create-subject.dto.ts
@@ -1,7 +1,9 @@
+import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateSubjectDto {
   @IsNotEmpty()
   @IsString()
+  @Transform(({ value }) => value.trim().toLowerCase())
   subjectName: string;
 }
