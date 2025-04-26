8edfd0b7d71b7c48fa6129fb11bfa11edb5ce08d
dwarowski <realcarenthusiast@yandex.ru>
2025-04-26 21:42:42 +0300

    Update create-tag.dto.ts

--- src/features/tags/create-tag/create-tag.dto.ts
+++ src/features/tags/create-tag/create-tag.dto.ts
@@ -1,7 +1,11 @@
-import { IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsNumber, IsString } from 'class-validator';
 
 export class CreateTagDto {
   @IsString()
+  @Transform(({ value }) => value.trim().toLowerCase())
   tag: string;
+  
+  @IsNumber()
   testId: number;
 }
