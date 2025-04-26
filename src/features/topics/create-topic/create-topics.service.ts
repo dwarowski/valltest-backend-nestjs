fec77857c5f722bd83a0fa71f355ff4f60af8c9b
dwarowski <realcarenthusiast@yandex.ru>
2025-04-26 21:06:51 +0300

    fix: proper defeniton of dto

--- src/features/topics/create-topic/create-topics.service.ts
+++ src/features/topics/create-topic/create-topics.service.ts
@@ -17,7 +17,7 @@ export class CreateTopicService {
 
   // Создание темы
   async execute(createTopicDto: CreateTopicDto) {
-    const subject = await this.getSubject.execute(createTopicDto.subjectName);
+    const subject = await this.getSubject.execute({subjectName: createTopicDto.subjectName});
     const topicEntity = await this.topicRepository.findOneBy({
       topicName: createTopicDto.topicName,
     });
