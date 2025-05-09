f34aa3ea81f7b9412321c04c92cea7c1c4be706f
dwarowski <realcarenthusiast@yandex.ru>
2025-05-10 02:03:14 +0300

    add: more secure dtos

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.use(cookieParser());
 
