ed6a97bd805acab7fdfdf70fcc14847c52702f22
dwarowski <realcarenthusiast@yandex.ru>
2025-05-10 02:29:08 +0300

    add: documented

--- src/routes/subjects/subjects.controller.ts
+++ src/routes/subjects/subjects.controller.ts
@@ -4,6 +4,7 @@ import { CreateSubjectDto } from '../../features/subjects/create-subject/create-
 import { SubjectEntity } from '../../entities/subjects/subject.entity';
 import { CreateSubjectService } from 'src/features/subjects/create-subject/create-subject.service';
 import { DeleteSubjectService } from 'src/features/subjects/delete-subject/delete-subject.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('subjects')
 export class SubjectController {
@@ -14,12 +15,20 @@ export class SubjectController {
 
   // Создать предмет
   @Post()
+  @ApiOperation({
+    summary: 'Create subject',
+    description: 'Create unique subject',
+  })
   async create(@Body() subjectData: CreateSubjectDto): Promise<SubjectEntity> {
     return this.creaeteSubjectService.execute(subjectData);
   }
 
   // Удалить предмет
   @Delete(':subject')
+  @ApiOperation({
+    summary: 'Delete subject',
+    description: 'Delete subject by name',
+  })
   async delete(@Param('subject') subject: string): Promise<string> {
     return await this.deleteSubjectSerivce.execute({ subjectName: subject });
   }
