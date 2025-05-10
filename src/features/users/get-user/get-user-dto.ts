3778add8756d5d7c1ab818a9a1e837ec47a1b6e0
dwarowski <realcarenthusiast@yandex.ru>
2025-05-11 00:08:45 +0300

    add: changed userdto and func fix

--- src/features/users/get-user/get-user-dto.ts
+++ src/features/users/get-user/get-user-dto.ts
@@ -1,8 +1,9 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UserDto {
   @IsString()
-  id: string;
-  @IsString()
+  @IsNotEmpty()
   username: string;
+  @IsString()
+  avatar_location: string | null;
 }
--- src/features/users/get-user/get-user.service.ts
+++ src/features/users/get-user/get-user.service.ts
@@ -13,21 +13,28 @@ export class GetUserService {
   ) {}
 
   // Метод для получения пользователя по айди
-  async execute(identifier: string, type: 'id' | 'username'): Promise<UserDto> {
+  async execute(identifier: string, type: 'id'): Promise<User>;
+  async execute(identifier: string, type: 'username'): Promise<UserDto>;
+  async execute(identifier: string, type: 'id' | 'username') {
     const whereClause =
       type === 'username'
         ? { where: { username: identifier } }
         : { where: { id: identifier } };
 
-    const user = await this.usersRepository.findOne(whereClause);
-    if (!user) {
+    const userEntity = await this.usersRepository.findOne(whereClause);
+    if (!userEntity) {
       throw new NotFoundException('User not found');
     }
 
-    if (!user.username) {
-      user.username = '';
+    if (type === 'username') {
+      const user: UserDto = {
+        username: userEntity.username,
+        avatar_location: userEntity.avatar_location,
+      };
+
+      return user;
+    } else {
+      return userEntity;
     }
-    const userCleaned: UserDto = { id: user.id, username: user.username };
-    return userCleaned;
   }
 }
