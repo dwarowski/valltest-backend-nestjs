364f955d11af1b7fc6484d8e33ccdcc42f102caf
dwarowski <realcarenthusiast@yandex.ru>
2025-05-10 17:03:38 +0300

    fix: correct transform exception

--- src/features/topics/create-topic/create-topic.dto.ts
+++ src/features/topics/create-topic/create-topic.dto.ts
@@ -4,11 +4,21 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateTopicDto {
   @IsNotEmpty()
   @IsString()
-  @Transform(({ value }) => value.trim().toLowerCase())
+  @Transform(({ value }) => {
+    if (typeof value === 'string') {
+      return value.trim().toLowerCase();
+    }
+    return value;
+  })
   topicName: string;
 
   @IsNotEmpty()
   @IsString()
-  @Transform(({ value }) => value.trim().toLowerCase())
+  @Transform(({ value }) => {
+    if (typeof value === 'string') {
+      return value.trim().toLowerCase();
+    }
+    return value;
+  })
   subjectName: string;
 }
--- src/features/topics/delete-topic/delete-topic.dto.ts
+++ src/features/topics/delete-topic/delete-topic.dto.ts
@@ -4,6 +4,11 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class DeleteTopicDto {
   @IsNotEmpty()
   @IsString()
-  @Transform(({ value }) => value.trim().toLowerCase())
+  @Transform(({ value }) => {
+    if (typeof value === 'string') {
+      return value.trim().toLowerCase();
+    }
+    return value;
+  })
   topicName: string;
 }
