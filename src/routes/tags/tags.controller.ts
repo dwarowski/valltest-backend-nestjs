31db86ce7f8be39417b188e96cc0eed21c75aecb
dwarowski <realcarenthusiast@yandex.ru>
2025-05-10 16:29:38 +0300

    fix: using dto for delete

--- src/routes/tags/tags.controller.ts
+++ src/routes/tags/tags.controller.ts
@@ -1,9 +1,10 @@
-import { Post, Body, Controller, Delete, Param } from '@nestjs/common';
+import { Post, Body, Controller, Delete } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { CreateTagDto } from '../../features/tags/create-tag/create-tag.dto';
 import { CreateTagService } from 'src/features/tags/create-tag/create-tag.service';
 import { DeleteTagService } from 'src/features/tags/delete-tag/delete-tag.service';
+import { DeleteTagDto } from 'src/features/tags/delete-tag/delete-tag.dto';
 
 @Controller('tags')
 @ApiTags('Tags')
@@ -22,12 +23,12 @@ export class TagsController {
     return this.createTagService.execute(dto);
   }
 
-  @Delete(':tag')
+  @Delete()
   @ApiOperation({
     summary: 'Delete tag',
     description: 'Delete tag',
   })
-  deleteTag(@Param('tag') tag: string) {
-    return this.deleteTagService.execute({ tag });
+  deleteTag(@Body() tagDto: DeleteTagDto) {
+    return this.deleteTagService.execute(tagDto);
   }
 }
