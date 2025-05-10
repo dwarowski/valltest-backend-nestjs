e8dd9a0634fe6126754637a697235cf3cb02817f
dwarowski <realcarenthusiast@yandex.ru>
2025-05-10 16:29:08 +0300

    fix: create tag not relation and tag

--- src/features/tags/create-tag/create-tag.service.ts
+++ src/features/tags/create-tag/create-tag.service.ts
@@ -1,30 +1,18 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { CreateTagDto } from './create-tag.dto';
 import { TagsEntity } from '../../../entities/tags/tags.entity';
-import { CreateRelationTestTagService } from 'src/features/test-tag/create-relation/create-relation-test-tag.service';
-import { GetTestsIdService } from 'src/features/tests/get-test-id/get-tests-id.service';
 
 @Injectable()
 export class CreateTagService {
   constructor(
-    @Inject(GetTestsIdService)
-    private readonly getTest: GetTestsIdService,
     @InjectRepository(TagsEntity)
     private readonly tagsRepository: Repository<TagsEntity>,
-    @Inject(CreateRelationTestTagService)
-    private readonly createRelationTestTag: CreateRelationTestTagService,
   ) {}
 
   async execute(dto: CreateTagDto) {
-    const { tag, testId } = dto;
-    const testEntity = await this.getTest.execute(testId, 'entity');
-
-    const tags = await this.tagsRepository.save({
-      tag,
-    });
-    return await this.createRelationTestTag.execute(testEntity, tags);
+    await this.tagsRepository.save({ tag: dto.tag });
   }
 }
