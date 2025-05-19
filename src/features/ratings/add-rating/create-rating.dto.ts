78b22315896e03a6134838691e915793f5ca4654
dwarowski <realcarenthusiast@yandex.ru>
2025-05-19 21:34:44 +0300

    refactor: validation fix from int to number

--- src/features/ratings/add-rating/create-rating.dto.ts
+++ src/features/ratings/add-rating/create-rating.dto.ts
@@ -1,19 +1,19 @@
 import {
   IsNotEmpty,
-  IsInt,
   Min,
   Max,
   IsString,
   IsOptional,
+  IsNumber,
 } from 'class-validator';
 
 export class CreateRatingDto {
   @IsNotEmpty()
-  @IsInt()
+  @IsNumber()
   testId: number; // ID теста
 
   @IsNotEmpty()
-  @IsInt()
+  @IsNumber()
   @Min(1)
   @Max(5)
   rating: number; // Оценка от 1 до 5
