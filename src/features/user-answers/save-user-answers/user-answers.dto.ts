a062470f73ba8b1e5941171ea6c0665212dd67be
dwarowski <realcarenthusiast@yandex.ru>
2025-05-21 15:40:18 +0300

    refactor: better dto validation

--- src/features/user-answers/save-user-answers/user-answers.dto.ts
+++ src/features/user-answers/save-user-answers/user-answers.dto.ts
@@ -1,8 +1,11 @@
-import { IsNumber } from 'class-validator';
+import { ArrayNotEmpty, IsArray, IsNumber } from 'class-validator';
 
 export class UserAnswersDto {
   @IsNumber()
   testId: number;
+
+  @IsArray()
+  @ArrayNotEmpty()
   userAnswers: {
     problemId: number;
     answerId: number;
