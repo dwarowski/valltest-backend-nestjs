46f00bdd502a00391c3c944360b5360b0ddace67
dwarowski <realcarenthusiast@yandex.ru>
2025-05-22 18:01:04 +0300

    fix: new saving mechanic and check

--- src/features/user-answers/save-user-answers/save-user-answers.service.ts
+++ src/features/user-answers/save-user-answers/save-user-answers.service.ts
@@ -1,6 +1,6 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { Request } from 'express';
-import { UserAnswersDto } from './user-answers.dto';
+import { UserAnswersDto } from '../get-user-test-answers/user-answers.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserAnswersEntity } from 'src/entities/user-answers/user-answer.entity';
 import { Repository } from 'typeorm';
@@ -10,6 +10,8 @@ import { GetUserService } from 'src/features/users/get-user/get-user.service';
 import { GetProblemService } from 'src/features/problems/get-problem/get-problem.service';
 import { GetAnswerService } from 'src/features/answers/get-answer/get-answer.service';
 import { GetUserTestAnsweresService } from '../get-user-test-answers/get-user-test-answers.service';
+import { TestsEntity } from 'src/entities/tests/test.entity';
+import { User } from 'src/entities/users/user.entity';
 
 @Injectable()
 export class SaveUserAnswersService {
@@ -33,37 +35,79 @@ export class SaveUserAnswersService {
     const payload = await extractToken(req);
     const userId = payload.id;
 
-    const userEntity = await this.getUser.execute(userId, 'id');
-    const testsEntity = await this.getTest.execute(testId);
+    // Получение пользователя и теста
+    const [userEntity, testsEntity] = await Promise.all([
+      this.getUser.execute(userId, 'id'),
+      this.getTest.execute(testId),
+    ]);
 
-    const checkUserAnswers = await this.getUserTestAnwers.execute(
-      userEntity.id,
-      testsEntity.id,
+    const checkUserAnswers = await this.CheckUserAnswers(
+      userEntity,
+      testsEntity,
     );
-    if (checkUserAnswers != 'No Answers Found') {
-      throw new Error('Вы уже проходили этот тест');
-    }
+
+    // Trying to find exsiting answers
+    const existingAnswers = checkUserAnswers
+      ? await this.userAnswersRepository.find({
+          where: {
+            user: { id: userEntity.id },
+            test: { id: testsEntity.id },
+          },
+          relations: ['problem', 'answer', 'user', 'test'],
+        })
+      : [];
+
+    // Создаем карту существующих ответов для быстрого поиска
+    const existingAnswersMap = new Map<number, UserAnswersEntity>();
+    existingAnswers.forEach((ans) => {
+      existingAnswersMap.set(ans.problem.id, ans);
+    });
 
     await Promise.all(
       userAnswers.map(async (userAnswer) => {
-        try {
-          const problemEntity = await this.getProblem.execute(
-            userAnswer.problemId,
-          );
-          const answerEntity = await this.getAnswer.execute(
-            userAnswer.answerId,
-          );
+        const [problemEntity, answerEntity] = await Promise.all([
+          this.getProblem.execute(userAnswer.problemId),
+          this.getAnswer.execute(userAnswer.answerId),
+        ]);
 
+        const existingAnswer = existingAnswersMap.get(problemEntity.id);
+
+        if (existingAnswer) {
+          // Обновляем существующий ответ, если он отличается
+          if (existingAnswer.answer.id !== answerEntity.id) {
+            existingAnswer.answer = answerEntity;
+            await this.userAnswersRepository.save(existingAnswer);
+          }
+        } else {
+          // Создаем новый ответ
           await this.userAnswersRepository.save({
             user: userEntity,
             test: testsEntity,
             problem: problemEntity,
             answer: answerEntity,
           });
-        } catch (error) {
-          throw error;
         }
       }),
     );
+  }
+
+  private async CheckUserAnswers(
+    userEntity: User,
+    testsEntity: TestsEntity,
+  ): Promise<UserAnswersDto | null> {
+    let checkUserAnswers: UserAnswersDto | null = null;
+    try {
+      checkUserAnswers = await this.getUserTestAnwers.execute(
+        userEntity.id,
+        testsEntity.id,
+      );
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        checkUserAnswers = null;
+      } else {
+        throw error;
+      }
+    }
+    return checkUserAnswers;
   }
 }
