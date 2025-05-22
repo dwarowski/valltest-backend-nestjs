c400c8014517e95ccf62efdf62405bb0a9cdfe23
dwarowski <realcarenthusiast@yandex.ru>
2025-05-22 16:35:58 +0300

    refactor: cleaner code and get test from repository

--- src/features/tests/get-test-correct-answers/get-test-corect-answers.service.ts
+++ src/features/tests/get-test-correct-answers/get-test-corect-answers.service.ts
@@ -1,26 +1,62 @@
-import { BadRequestException, Inject, Injectable } from '@nestjs/common';
-import { GetTestsIdService } from '../get-test-id/get-tests-id.service';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { TestCorrectAnswersDto } from './test-correct-answers.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TestsEntity } from 'src/entities/tests/test.entity';
+import { Repository } from 'typeorm';
+import { CorrectAnswerDto } from './correct-answer.dto';
 
 @Injectable()
 export class GetTestCorrectAnswersService {
   constructor(
-    @Inject(GetTestsIdService)
-    private readonly getTests: GetTestsIdService,
+    @InjectRepository(TestsEntity)
+    private readonly testsRepository: Repository<TestsEntity>,
   ) {}
 
   async execute(testId: number) {
-    const testEntity = await this.getTests.execute(testId, 'entity');
-    const correctAnswers = await Promise.all(
-      testEntity.problems.map(async (problem) => {
-        const correctAnswer = problem.answers.find(
-          (answer) => answer.is_correct,
+    const testEntity = await this.testsRepository
+      .createQueryBuilder('test')
+      .where({ id: testId })
+      .leftJoinAndSelect('test.problems', 'problems')
+      .leftJoinAndSelect('problems.answers', 'answers')
+      .addSelect('answers.is_correct')
+      .getOne();
+
+    if (!testEntity) {
+      throw new NotFoundException(`Test with id: ${testId}`);
+    }
+
+    const { problems } = testEntity;
+    if (!problems) {
+      throw new InternalServerErrorException('Problems not loaded');
+    }
+
+    const correctAnswers: CorrectAnswerDto[] = problems.map((problem) => {
+      const { answers } = problem;
+
+      if (!answers) {
+        throw new InternalServerErrorException('Answers not loaded');
+      }
+
+      const correctAnswerEntity = answers.find((answer) => answer.is_correct);
+
+      if (!correctAnswerEntity) {
+        throw new InternalServerErrorException(
+          `No correct answer found for problem with ID: ${problem.id}`,
         );
-        if (!correctAnswer) {
-          throw new BadRequestException('No Correct Answer???'); //TODO make at least one correct answer neccessary
-        }
-        return { problemId: problem.id, answerId: correctAnswer.id };
-      }),
-    );
-    return { testId: testEntity.id, correctAnswers: correctAnswers };
+      }
+
+      return { problemId: problem.id, answerId: correctAnswerEntity.id };
+    });
+
+    const result: TestCorrectAnswersDto = {
+      testId: testEntity.id,
+      correctAnswers,
+    };
+
+    return result;
   }
 }
