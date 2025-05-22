57614c4c83105bfa3ec300aa66925f46a75874db
dwarowski <realcarenthusiast@yandex.ru>
2025-05-22 18:01:48 +0300

    refactor: simplified logic

--- src/features/user-answers/get-user-test-answers/get-user-test-answers.service.ts
+++ src/features/user-answers/get-user-test-answers/get-user-test-answers.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserAnswersEntity } from 'src/entities/user-answers/user-answer.entity';
 import { Repository } from 'typeorm';
+import { UserAnswersDto } from './user-answers.dto';
 
 @Injectable()
 export class GetUserTestAnsweresService {
@@ -10,25 +11,26 @@ export class GetUserTestAnsweresService {
     private readonly userAnswersRepository: Repository<UserAnswersEntity>,
   ) {}
 
-  async execute(userId: string, testId: number) {
-    const userAnswers = await this.userAnswersRepository.find({
+  async execute(userId: string, testId: number): Promise<UserAnswersDto> {
+    const userAnswersEnitiy = await this.userAnswersRepository.find({
       where: { user: { id: userId }, test: { id: testId } },
       relations: ['test', 'user', 'problem', 'answer'],
     });
 
-    const userAnswersFilterd = await Promise.all(
-      userAnswers.map(async (userAnswer) => {
-        const { problem, answer } = userAnswer;
-        return { problemId: problem.id, answerId: answer.id };
-      }),
-    );
-    if (userAnswers.length == 0) {
-      return 'No Answers Found';
-    } else {
-      return {
-        testId: userAnswers[0].test.id,
-        userAnswers: userAnswersFilterd,
-      };
+    if (userAnswersEnitiy.length == 0) {
+      throw new NotFoundException(
+        `No answers found for test with id: ${testId}`,
+      );
     }
+
+    const userAnswers = userAnswersEnitiy.map((userAnswer) => ({
+      problemId: userAnswer.problem.id,
+      answerId: userAnswer.answer.id,
+    }));
+
+    return {
+      testId: userAnswersEnitiy[0].test.id,
+      userAnswers,
+    };
   }
 }
